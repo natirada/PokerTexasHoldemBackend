b2cc39656aaf9c5cd2b6ab84c3b59f2d6d48f881
netanel.rada <netanel.rada@herolo.co.il>
2020-11-28 14:49:41 +0200

    last changes

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ server.listen(port);
 //const io = require('./socket/socket').init(server);
 socket.init(server);
 socket.connection();
+
+// "scripts": {
+// 	"start": "MONGO_ATLAS_PASSWORD=aa9432651 JWT_KEY=1234567 SENDGRID_API_KEY=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 node server.js"
+// },
